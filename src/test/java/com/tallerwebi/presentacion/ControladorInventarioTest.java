a8d3bea3a294879d7a1ce078b42dd2fdf4dc8858
sjgar98 <54064646+sjgar98@users.noreply.github.com>
2025-05-23 16:34:41 -0300

    Feat: Home/Inventario - Codigo inicial (#1)
    * Feat: Home/Inventario - Template, controller, service, repository, entities
    
    * Fix: Array reversing en Java 11
    
    * Fix: Split de servicios y unit tests

---
+++ src/test/java/com/tallerwebi/presentacion/ControladorInventarioTest.java
@@ -0,0 +1,37 @@
+package com.tallerwebi.presentacion;
+
+import com.tallerwebi.dominio.Jugador;
+import com.tallerwebi.dominio.ServicioJugador;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.text.IsEqualIgnoringCase.equalToIgnoringCase;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ControladorInventarioTest {
+    private ControladorInventario controladorInventario;
+    private HttpServletRequest requestMock;
+    private HttpSession sessionMock;
+    private ServicioJugador servicioJugadorMock;
+
+    @BeforeEach
+    public void init() {
+        requestMock = mock(HttpServletRequest.class);
+        sessionMock = mock(HttpSession.class);
+        servicioJugadorMock = mock(ServicioJugador.class);
+        controladorInventario = new ControladorInventario(servicioJugadorMock);
+    }
+
+    @Test
+    public void devuelveModelAndView() {
+        when(requestMock.getSession()).thenReturn(sessionMock);
+        when(servicioJugadorMock.getJugadorActual()).thenReturn(new Jugador());
+        ModelAndView modelAndView = controladorInventario.getInventario(requestMock);
+        assertThat(modelAndView.getViewName(), equalToIgnoringCase("inventario"));
+    }
+}
